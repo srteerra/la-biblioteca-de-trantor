81b24249bd350cf319c1f15f9f2fc59848a71e88
jonocrod12 <jon_ocampo@hotmail.com>
2021-09-28 14:20:53 -0600

    hola

--- README.md
+++
@@ -1,1 +0,0 @@
-# la-biblioteca-de-trantor
--- src/app/index.js
+++
@@ -1,9 +0,0 @@
-'use-strict'
-import Vue from 'vue';
-import App from './App.vue'
-import router from './routes'
-
-new Vue({
-    router,
-    render:h=>h(App)
-}).$mount('#app')

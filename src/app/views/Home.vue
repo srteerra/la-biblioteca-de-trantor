f52a100c8bab75a92fc4eeb8e3f4a8418935c62f
Angel Lopez <srterradev@gmail.com>
2021-10-01 16:06:06 -0600

    changes to the colors, dark and primary

--- src/app/views/Home.vue
+++ src/app/views/Home.vue
@@ -18,11 +18,20 @@
             <h5 class="modal-title" id="rewardsModalLabel">Recompensas</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
-          <div class="modal-body">
-            ...
+          <div class="modal-body p-5">
+            <h5>Ago - Feb</h5>
+            <h3 class="mb-5">Estas son las recompensas:</h3>
+            <p class="text-dark text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore perferendis consectetur voluptas itaque saepe.</p>
+            <ol>
+              <li>Objecto</li>
+              <li>Objecto</li>
+              <li>Objecto</li>
+              <li>Objecto</li>
+            </ol>
+            <p class="text-dark text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore perferendis consectetur voluptas itaque saepe.</p>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-primary text-white" data-bs-dismiss="modal">Cerrar</button>
+            <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Entendido</button>
           </div>
         </div>
       </div>

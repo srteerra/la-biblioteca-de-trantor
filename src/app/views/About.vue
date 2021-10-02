0c063d325014d94099e33cefb2db1353c8be4160
Guapura89 <betocrack1228@gmail.com>
2021-10-01 18:17:01 -0600

    Starting About view

--- src/app/views/About.vue
+++ src/app/views/About.vue
@@ -1,15 +1,47 @@
 <template>
   <div>
-      <h2>About us</h2>
+    <div class="container">
+      <p class="title fs-1 fw-bold text-uppercase">Acerca</p>
+      <div class="space"></div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque,
+        similique veritatis quod mollitia assumenda autem qui quaerat tenetur
+        officiis, inventore aperiam quis suscipit! Similique atque libero natus
+        tenetur. Velit, adipisci!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis vel
+        quisquam ratione, ad nobis, beatae tempora quos quis nesciunt impedit
+        nihil pariatur, error porro eveniet quam officiis laudantium delectus
+        molestiae quo? Quas obcaecati a illum nesciunt doloribus dolore
+        provident deserunt dolorem dolores eligendi ipsum voluptates voluptatem
+        ipsam, ratione consectetur tempora assumenda incidunt quos aspernatur
+        error odio ipsa repudiandae quia. Officia!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi iste
+        iusto numquam magnam quas eos, cupiditate laudantium impedit odit
+        similique neque, dolorem quasi exercitationem, officia nesciunt ab? Ea
+        obcaecati alias velit laboriosam minima aut esse ad modi quibusdam!
+        Explicabo, distinctio molestiae voluptate atque cupiditate officiis modi
+        sapiente itaque nulla, laudantium numquam corrupti qui? Totam odit
+        dignissimos dolorem sed at cupiditate.
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name:"about"
-}
+  name: "about",
+};
 </script>
 
 <style lang="scss">
 
+.space{
+  border-top: 5px solid black;
+  width: 10%;
+  height: 30px;
+}
 </style> 

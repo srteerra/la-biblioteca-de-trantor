08b3ba1ff0a84c8b8ae8102be52da3aea37e466a
Angel Lopez <srterradev@gmail.com>
2021-10-04 13:51:21 -0600

    presentation section added

--- src/app/views/Home.vue
+++ src/app/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
-    <img src="../assets/img/planet-1.png" alt="" srcset="" id="planet-1">
-    <img src="../assets/img/planet-2.png" alt="" srcset="" id="planet-2">
+    <img src="../assets/img/planet-1.png" alt="" id="planet-1">
+    <img src="../assets/img/planet-2.png" alt="" id="planet-2">
 
     <div class="title__container">
       <h1 class="text-dark">LA BIBLIOTECA DE TRANTOR</h1>
@@ -11,6 +11,26 @@
     <Dashboard/>
     <div class="infotop__container">
       <p class="text-dark">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit quia dicta nam suscipit magni hic porro corporis, velit modi magnam, fugit aliquam obcaecati dolores praesentium temporibus asperiores minus! Totam, exercitationem.</p>
+    </div>
+    <div class="presentation__container">
+      <div class="presentation__grid">
+        <div class="presentation__whatis">
+          <h1 class="text-dark">¿Que es?</h1>
+          <p class="text-dark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque expedita iure quas laudantium libero nesciunt omnis eligendi! Unde quibusdam laudantium eius in dignissimos nam adipisci, illo labore temporibus nemo consequuntur.</p>
+          <p class="text-dark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque expedita iure quas laudantium libero nesciunt omnis eligendi! Unde quibusdam laudantium eius in dignissimos nam adipisci, illo labore temporibus nemo consequuntur.</p>
+          <router-link to="/" class="btn btn-outline-dark py-3 px-4 mt-3 rounded-pill">Explora mas <span><i class="fas fa-arrow-right ps-2"></i></span></router-link>
+        </div>
+        <div class="presentation__whatis__image">
+          <img src="../assets/img/planets-1.png" alt="" id="planets-1">
+        </div>
+        <div class="presentation__why__image">
+          <img src="../assets/img/planets-2.png" alt="" id="planets-2">
+        </div>
+        <div class="presentation__why">
+          <h1 class="text-dark">¿Por que?</h1>
+          <p class="text-dark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque expedita iure quas laudantium libero nesciunt omnis eligendi! Unde quibusdam laudantium eius in dignissimos nam adipisci, illo labore temporibus nemo consequuntur.</p>
+        </div>
+      </div>
     </div>
 
     <!-- Modal -->
@@ -48,6 +68,10 @@
 
   export default {
     name: 'home',
+    data() {
+      return {
+      }
+    },
     components: {
       Dashboard
     }
@@ -59,7 +83,7 @@
     width: 100%;
     height: auto;
     display: grid;
-    grid-template-columns: .4fr .3fr 1fr .3fr .4fr;
+    grid-template-columns: .4fr .4fr 1fr .4fr .4fr;
     background-color: #f5fce2;
 
     #planet-1 {
@@ -75,6 +99,16 @@
       position: absolute;
       left: 10%;
       top: 130%;
+      opacity: 40%;
+    }
+
+    #planets-1 {
+      max-width: 500px;
+      opacity: 40%;
+    }
+
+    #planets-2 {
+      max-width: 500px;
       opacity: 40%;
     }
 
@@ -112,6 +146,60 @@
       text-align: center;
       margin: 0 auto;
       padding: 50px 0px 150px;
+    }
+
+    .presentation__container {
+      width: 100%;
+      height: auto;
+      background-color: white;
+      grid-column: 1/6;
+      padding: 200px;
+
+      .presentation__grid {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: 1fr .2fr 1fr;
+
+        .presentation__whatis {
+          text-align: start;
+          grid-row: 1;
+
+          h1 {
+            padding-bottom: 50px;
+            font-family: 'Times New Roman', Times, serif;
+            font-size: 5em;
+          }
+
+          p {
+            line-height: 40px;
+          }
+        }
+        
+        .presentation__whatis__image {
+          grid-row: 1;
+          justify-self: center;
+        }
+
+        .presentation__why__image {
+          grid-row: 3;
+          justify-self: start;
+        }
+
+        .presentation__why {
+          text-align: end;
+          grid-row: 3;
+
+          h1 {
+            padding-bottom: 50px;
+            font-family: 'Times New Roman', Times, serif;
+            font-size: 5em;
+          }
+
+          p {
+            line-height: 40px;
+          }
+        }
+      }
     }
   }
 </style>

f4f73584b507e99eceaa5e5c415ad3605121ae98
Guapura89 <betocrack1228@gmail.com>
2021-10-17 20:30:35 -0600

    Starting sponsorship view

---
+++ src/app/views/Sponsorship.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="main__container">
+    <div class="container first__container">
+      <h1 class="text-dark fs-1 fw-bold">FOR SPONSORSHIP</h1>
+      <div class="space__div"></div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+        architecto accusantium, commodi similique placeat tenetur numquam
+        officia, laborum harum nobis at ipsam inventore provident aperiam porro
+        iusto. Eos magni, maxime officiis praesentium reiciendis consequatur
+        molestiae atque, error provident commodi quibusdam.<br /><br />
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam
+        laudantium debitis non, earum praesentium quo eveniet qui asperiores!
+        Aliquam ipsam veritatis temporibus qui, totam ad esse nostrum modi.
+        Nostrum, repellendus aperiam id corporis iusto voluptatem ducimus alias
+        unde laboriosam, ad, dolor porro aut. Ipsum nulla sit, consequuntur
+        inventore nesciunt fugit, facilis facere, molestias quos sapiente vitae
+        laboriosam fuga sequi officiis. Consequatur itaque nulla vero
+        praesentium reprehenderit architecto sed libero, iusto eaque deserunt
+        excepturi blanditiis illo porro recusandae repellat quis. Temporibus
+        nobis sapiente obcaecati dicta quas dolores error sit accusamus ea
+        repudiandae eos totam, excepturi, at ut voluptatibus incidunt. Eius,
+        praesentium.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "sponsorship",
+  dada() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss">
+.main__container {
+  padding-top: 300px;
+  .first__container {
+    padding: 0 10px 200px;
+
+    .space__div {
+      border-top: 2px solid black;
+      width: 15%;
+      height: 30px;
+    }
+  }
+}
+</style>

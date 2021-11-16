cabb3fe1098e9e7ef097452305f9f0780fa61068
Angel Lopez <srterradev@gmail.com>
2021-11-15 17:46:47 -0700

    fixing the fkg simple error on loans var

--- src/app/components/Loans-Dashboard.vue
+++ src/app/components/Loans-Dashboard.vue
@@ -6,15 +6,13 @@
         <thead>
           <tr>
             <th>ID</th>
-            <th>Titulo del libro</th>
             <th>Fecha en uso</th>
             <th>Estado del permiso</th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="loan in loans1" v-bind:key="loan">
+          <tr v-for="loan in loans" v-bind:key="loan">
             <td>{{loan.loan_id}}</td>
-            <td>{{loan.book_id}}</td>
             <td>{{loan.loan_dateOrdered}}</td>
             <td>{{loan.loan_status}}</td>
           </tr>
